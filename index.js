a8d581e96cd756c864387ab6c5b0c41d2bc623c8
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-07-26 20:54:30 +0200

    marking & js

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const submitBtn = document.querySelector(".submit-btn");
 const succesMassage = document.querySelector(
   ".success-message-modal-container"
 );
+const successBtn = document.querySelector(".succes-message-btn");
 
 submitBtn.addEventListener("click", onClick);
 
 function onClick() {
-  singUp.classList.add("");
+  singUp.classList.add("is-hiden");
+  succesMassage.classList.remove("is-hiden");
+  return;
+}
+
+succesMassage.addEventListener("click", outClick);
+
+function outClick() {
+  succesMassage.classList.add("is-hiden");
+  singUp.classList.remove("is-hiden");
+  return;
 }
